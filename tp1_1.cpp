98dc5ea7f7287940e73c3008295a478e66e5f52f
Marcos Lopez <marcosleonellopez@gmail.com>
2024-03-26 17:03:28 -0300

    archivo cpp modif.

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    int numero, *puntNum;
+    numero = 18;
+    puntNum = &numero;
+
+    printf("Contenido del puntero: %d\n", *puntNum);
+    printf("La dirección de memoria almacenada por el puntero: %p\n", puntNum);
+    printf("La dirección de memoria de la variable: %p\n", numero);
+
+
+
+
     printf("Hola Mundo!\n");
     return 0;
 }
