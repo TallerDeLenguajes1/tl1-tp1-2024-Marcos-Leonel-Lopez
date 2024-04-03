a255d91d72f2759e312bffa9f0f52904a1d16a17
Marcos Lopez <marcosleonellopez@gmail.com>
2024-04-03 11:16:46 -0300

    3e

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -7,7 +7,9 @@ int main(){
 
     printf("Contenido del puntero: %d\n", *puntNum);
     printf("La dirección de memoria almacenada por el puntero: %p\n", puntNum);
-    printf("La dirección de memoria de la variable: %p\n", numero);
+    printf("La dirección de memoria de la variable: %p\n", &numero);
+    printf("la dirección de memoria del puntero: %p\n", &puntNum);
+    printf("el tamaño de memoria utilizado por esa variable: %zu bytes \n", sizeof(puntNum));
 
 
 
